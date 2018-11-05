e361a1327720ca9356de25c3d17a6c40494d8259
1870571 <1870571@hcmut.edu.vn>
2018-11-05 22:24:55 +0700

    Add UI Source

---
+++ HMS-Web/src/app/layout/components/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+    pushRightClass: string = 'push-right';
+
+    constructor() {
+
+    }
+
+    ngOnInit() {}
+
+    isToggled(): boolean {
+        const dom: Element = document.querySelector('body');
+        return dom.classList.contains(this.pushRightClass);
+    }
+
+    toggleSidebar() {
+        const dom: any = document.querySelector('body');
+        dom.classList.toggle(this.pushRightClass);
+    }
+
+    // rltAndLtr() {
+    //     const dom: any = document.querySelector('body');
+    //     dom.classList.toggle('rtl');
+    // }
+
+    onLoggedout() {
+        localStorage.removeItem('isLoggedin');
+    }
+}
