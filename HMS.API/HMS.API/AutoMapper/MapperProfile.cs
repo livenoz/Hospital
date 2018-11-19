d6f9ee3dcfb70e32507308baa5bbb16827fe0c36
livenoz <huynhnguyenvietthanh1824@gmail.com>
2018-11-19 19:30:43 +0700

    -Add file mapper profile

---
+++ HMS.API/HMS.API/AutoMapper/MapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HMS.Entities.Models;
+using HMS.Common.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HMS.API.AutoMapper
+{
+    public class MapperProfile: Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<TPatient, PatientDto>();
+        }
+    }
+}
---
+++ HMS.API/HMS.API/Controllers/V1/PatientController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HMS.Business.Interfaces;
+using HMS.Business.Interfaces.Paginated;
+using HMS.Common.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HMS.API.Controllers.V1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly IPatientBusiness _patientBusiness;
+
+        public PatientController(IPatientBusiness patientBusiness)
+        {
+            _patientBusiness = patientBusiness;
+        }
+
+        // GET: api/Patient
+        [HttpGet]
+        public async Task<IPaginatedList<PatientDto>> Get(int pageIndex, int pageSize)
+        {
+            return await _patientBusiness.GetAll(pageIndex, pageSize);
+        }
+
+        // GET: api/Patient/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Patient
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Patient/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
